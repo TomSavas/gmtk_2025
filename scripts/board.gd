d27a43bfcb24d1e83d84b2140e6884da7a49eccc
TomSavas <tomas.savickas@protonmail.com>
2024-08-17 15:05:33 +0000

    Implement clearing

--- scripts/board.gd
+++ scripts/board.gd
@@ -4,7 +4,7 @@ extends Node
 signal should_step(depth: int)
 
 @export var time_dilation := 10.0
-@export var step_every_num_sec := 0.3
+@export var step_every_num_sec := 0.2
 @export var time_elapsed := 0.0
 
 static var singleton = null
@@ -17,11 +17,51 @@ static func instance() -> Board:
 var tetrominos: Array[Tetromino] = []
 var board: Array[bool] = []
 
+var currently_live_tetromino = null
+
 func _ready():
 	singleton = self
 	
 	board.resize(width * height)
 	board.fill(false)
+
+func _check_lines():
+	_recalc_board([currently_live_tetromino])
+	
+	var lines = []
+	for y in height:
+		var full = true
+		for x in width:
+			var index = y * width + x
+			if not board[index]: 
+				full = false
+				break
+		
+		if full:
+			lines.append(y)
+			
+	return lines
+
+func _clear_dead_squares(min_line, max_line):
+	for t in tetrominos:
+		#if t.topLeftSquare[1] < min_line or t.topLeftSquare[1] > max_line:
+			#continue
+			
+		var i = 0
+		while i < len(t.squares):
+			var s = t.squares[i]
+			var y = t.topLeftSquare[1] + s.offsetInTetromino[1]
+			if min_line <= y and y <= max_line:
+				t.swap_remove_square(i)
+			else:
+				i += 1
+	
+func _move_all_dead_above(min_line, cleared_line_count):
+	for t in tetrominos:
+		if t.topLeftSquare[1] > min_line:
+			continue
+			
+		t.forcedStep(cleared_line_count)
 
 func _process(delta: float) -> void:
 	if time_elapsed > step_every_num_sec:
@@ -29,10 +69,18 @@ func _process(delta: float) -> void:
 		should_step.emit(0)
 		time_elapsed = 0
 		
-	var lines = _check_lines()
-	_clear_lines(lines)
-	
-	_recalc_board()
+		if currently_live_tetromino and not currently_live_tetromino.alive:
+			currently_live_tetromino = null
+		
+		var lines = _check_lines()
+		if len(lines) == 0:
+			lines = [0]
+		else:
+			_clear_dead_squares(lines.min(), lines.max())
+			_move_all_dead_above(lines.min(), lines.max() - lines.min() + 1)
+			print("clearing!")
+		
+		_recalc_board()
 	
 	print("Board:")
 	for y in height:
@@ -46,16 +94,10 @@ func _process(delta: float) -> void:
 	
 	time_elapsed += delta
 	
-func _check_lines():
-	return null
-	
-func _clear_lines(lines):
-	pass
-	
-func _recalc_board(exclude = null):
+func _recalc_board(excludes = null):
 	board.fill(false)
 	for tetromino in tetrominos:
-		if exclude != null and tetromino == exclude:
+		if excludes != null and tetromino in excludes:
 			continue
 		for square in tetromino.squares:
 			board[_index(tetromino, square)] = true
@@ -68,7 +110,7 @@ func in_bounds(t: Tetromino) -> bool:
 	return true
 	
 func collides(t: Tetromino) -> bool:
-	_recalc_board(t)
+	_recalc_board([t, currently_live_tetromino])
 	if not in_bounds(t):
 		return true
 		
@@ -85,5 +127,6 @@ func _index(t: Tetromino, s: Square) -> int:
 	
 func add_tetromino(t: Tetromino): 
 	tetrominos.append(t)
+	currently_live_tetromino = t
 	for square in t.squares:
 		board[_index(t, square)] = true
--- scripts/tetromino.gd
+++ scripts/tetromino.gd
@@ -3,6 +3,7 @@ extends Node3D
 
 @export var topLeftSquare: Vector2 = Vector2(0, 0)
 @export var squares: Array[Square] = []
+@export var alive: bool = true
 
 func _init(squares: Array[Square]) -> void:
 	self.squares = squares
@@ -11,17 +12,25 @@ func _init(squares: Array[Square]) -> void:
 func _ready() -> void:
 	Board.instance().should_step.connect(step)
 
-func step(depth: int):
-	self.position.y -= 1.0
-	topLeftSquare[1] += 1
+func forcedStep(step_count=1, undo=false):
+	self.position.y -= (-1.0 if undo else 1.0) * step_count
+	topLeftSquare[1] += (-1 if undo else 1) * step_count
+	
+func step(depth: int = 0):
+	forcedStep()
 	
 	# TODO(savas): add some grace period for rotations, etc.
 	var board := Board.instance()
 	if board.collides(self):
-		self.position.y += 1.0
-		topLeftSquare[1] -= 1
-
+		forcedStep(1, true)
 		board.should_step.disconnect(step)
+		alive = false
+
+func swap_remove_square(index: int):
+	var temp = squares[-1]
+	squares[-1] = squares[index]
+	squares[index] = temp
+	squares.pop_back().queue_free()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
